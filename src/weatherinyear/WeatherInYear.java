c8010c650bc9fa2bc1c6fcba85d74865e8f7606e
ilja.astasev <ilja.astasev@ivkhk.ee>
2020-09-16 13:55:13 +0300

    Налало проекта

---
+++ src/weatherinyear/WeatherInYear.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherinyear;
+
+/**
+ *
+ * @author user
+ */
+public class WeatherInYear {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][] nums2 = new int[11][];
+        nums2[0] = new int[31];
+        nums2[1] = new int[29];
+        nums2[2] = new int[31];
+        nums2[3] = new int[30];
+        nums2[4] = new int[31];
+        nums2[5] = new int[30];
+        nums2[6] = new int[31];
+        nums2[7] = new int[31];
+        nums2[8] = new int[30];
+        nums2[9] = new int[31];
+        nums2[10] = new int[30];
+        nums2[11] = new int[31];
+        System.out.println("погода: ",nums2[4]);
+        
+    }    
+
+        
+        
+        
+        
+                
+
+    }
+    
+}
